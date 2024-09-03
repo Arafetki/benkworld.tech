ed9bde27c3141e475f7a9880679f0209e337fa9d
Arafet Ben Kilani <arafet.benkilani@supcom.tn>
2024-09-03 23:55:42 +0100

    fixed ui bugs

--- src/components/navigation/main-nav.tsx
+++ src/components/navigation/main-nav.tsx
@@ -2,14 +2,11 @@
 
 import { useEffect } from "react";
 import Link from "next/link";
-import { usePathname } from 'next/navigation';
 import type { NavItem } from "@/lib/types"
 import { Icons } from "@/components/icons";
 import { ThemeSwitcher } from "@/components/theme-switcher";
 import { siteConfig } from "@/config/site";
 import { useMobileMenu } from "@/hooks/useMobileMenu";
-import { cn } from "@/lib/utils";
-import { useHash } from "@/hooks/useHash";
 
 type MainNavbarProps = {
     items?: Readonly<NavItem[]>
@@ -17,27 +14,28 @@ type MainNavbarProps = {
 
 export default function  MainNavbar({items}: MainNavbarProps) {
 
-    const {isMobileMenuOpen,toggle} = useMobileMenu();
-    const pathname = usePathname();
-    const hash = useHash();
-    const hashPath = hash && hash.length ? `/${hash}`: pathname;
+    const {isMobileMenuOpen, toggle, setFalse} = useMobileMenu();
+
 
     useEffect(()=>{
 
         if (isMobileMenuOpen) {
-            document.body.style.overflow = "hidden";
-        }else{
-            document.body.style.overflow = "unset";
+            document.body.style.overflow = 'hidden'
+        } else {
+            document.body.style.overflow = 'unset'
         }
 
-    },[isMobileMenuOpen]);
-
+    },[isMobileMenuOpen])
 
     return (
         <nav className="h-24 max-w-7xl mx-auto px-6 lg:px-8 flex items-center justify-between">
             <div className="flex items-center gap-5">
                 <div className="flex items-center gap-2">
-                    <Link href='/' className="order-2 text-primary text-nowrap font-bold tracking-tight" >
+                    <Link 
+                        href='/' 
+                        className="order-2 text-primary text-nowrap font-bold tracking-tight"
+                        onClick={()=>{if (isMobileMenuOpen) setFalse()}}
+                    >
                         {siteConfig.title}
                     </Link>
                     <button
@@ -51,14 +49,13 @@ export default function  MainNavbar({items}: MainNavbarProps) {
                     {items?.map(item=>{
                         return (
                             <li key={item.name}>
-                                <Link href={item.href} className={cn("hover:text-foreground text-sm", hashPath === item.href? 'text-foreground': 'text-foreground/60 ')}>{item.name}</Link>
+                                <Link href={item.href} className="text-foreground/60 hover:text-foreground text-sm">{item.name}</Link>
                             </li>
                         );
                     })}
                 </ul>
             </div>
             <div>
-                
                 <ThemeSwitcher/>
             </div>
         </nav>
