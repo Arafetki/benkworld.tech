97200497972d1bf4997e27be0a0d5e4440873421
Arafet Ben Kilani <arafet.benkilani@supcom.tn>
2024-09-04 16:51:32 +0100

    develop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import UnderConstruction from "@/components/under-construction";
-
 export default function HomePage() {
 
   return (
-    <div className="h-screen flex justify-center items-center">
-      <UnderConstruction/>
+    <div>
+      <section>Hero Section</section>
     </div>
   );
 }
