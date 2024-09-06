076a0ebab1d8ccd621ac5225793fbccb5a3eb69a
Arafet Ben Kilani <arafet.benkilani@supcom.tn>
2024-09-06 14:37:37 +0100

    turn the cache back on

--- src/app/(community)/blog/page.tsx
+++ src/app/(community)/blog/page.tsx
@@ -3,23 +3,18 @@ import Image from "next/image";
 import Link from 'next/link';
 import { getPublishedPosts } from "@/server/db/data/posts";
 import { cache, formatDate, estimateReadTimeMinutes, pluralize } from "@/utils";
-import type { SearchParams } from "@/types";
-
-type PageProps = {
-    searchParams: SearchParams
-}
 
 export const metadata: Metadata = {
     title: `Arafet's Blog`
 }
 
-// const getCachedPosts = cache( async ()=>{
-//     return await getPublishedPosts()
-// },['posts'],{revalidate: 60, tags: ['posts']})
+const getCachedPosts = cache( async () =>{
+    return await getPublishedPosts()
+},['posts'],{revalidate: 60, tags: ['posts']})
 
 export default async function Blog() {
 
-    const {posts,error} = await getPublishedPosts()
+    const {posts,error} = await getCachedPosts()
 
     if (error) return (<p className="text-xl text-red-500 font-medium tracking-tight">{error.message}</p>);
 
