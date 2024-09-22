3264ab7a9052cdb4a559ca624a58dc0c2d188cca
Arafet Ben Kilani <arafet.benkilani@supcom.tn>
2024-09-22 22:55:29 +0100

    enhanced footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,10 +1,15 @@
+import Link from "next/link";
 import { SocialMedia } from "@/components/social-media";
 
 export default function Footer() {
     return (
         <footer className="">
             <div className="max-w-7xl mx-auto p-6 lg:py-10 flex flex-col items-center gap-y-4 sm:flex-row sm:justify-between border-t-2">
-                <p className="text-nowrap">© {new Date().getFullYear()}. All rights reserved</p>
+                <div className="flex flex-col gap-y-1 text-center sm:text-start">
+                    <p className="text-sm text-nowrap text-muted-foreground">© Copyright {new Date().getFullYear()}. Arafet BenKilani</p>
+                    <p className="text-sm text-nowrap text-muted-foreground">Code snippets are <Link href="https://opensource.org/license/MIT" target="_blank" rel="noopener noreferrer" className="underline hover:text-accent-foreground">MIT licensed</Link></p>
+                    <Link href="/disclaimer" className="text-sm text-nowrap text-muted-foreground underline hover:text-accent-foreground">Disclaimer</Link>
+                </div>
                 <SocialMedia/>
             </div>
         </footer>
