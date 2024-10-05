a26b7b60ea6cf76312042e35893999768ce8dd16
Arafet Ben Kilani <arafet.benkilani@supcom.tn>
2024-10-05 15:45:49 +0100

    still working on hero section

--- src/app/(community)/page.tsx
+++ src/app/(community)/page.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import { Icons } from "@/components/icons";
 import { FlipWords } from "@/components/ui/flip-words";
 import { useMemo } from "react";
+import { BackgroundLines } from "@/components/ui/background-lines";
 
 export default function Home() {
 
@@ -14,69 +15,53 @@ export default function Home() {
 
   return (
     <div className="max-w-7xl w-full mx-auto">
-      <section id="hero" className="flex flex-col py-10 sm:p-20">
-        <div className="w-fit relative flex justify-center items-center pl-1 pb-8">
-          <div className="absolute size-[86px] rounded-full border-4 border-secondary border-double animate-spin" style={{animationDuration: "3s"}}/>
-          <Image
-            src={siteConfig.owner.photo}
-            alt="Me"
-            height={76}
-            width={76}
-            priority
-            className="rounded-full aspect-square object-cover border-2 border-secondary"
-          />
-        </div>
-        <div className="space-y-4">
-          <h2 className="font-semibold text-base lg:text-2xl text-pretty leading-[1.5]">
-            Hey, I&apos;m {`${siteConfig.owner.firstName} ${siteConfig.owner.lastName}`}
-          </h2>
-          <h1 className="font-bold text-[1.75rem] sm:text-3xl lg:text-6xl leading-[1.5]">
-            Cloud <br/> DevSecOps <span className="text-secondary">Engineer</span>
-          </h1>
-          <div className="text-muted-foreground xl:text-xl max-w-[60ch] space-y-1">
-            <div>
-              <span>I&apos;m passionate about,</span> <FlipWords words={words} className="p-0 text-sm xl:text-lg font-medium"/>
+      <section id="hero" className="relative py-8 lg:py-12">
+        <BackgroundLines className="flex flex-col sm:items-center gap-y-4 sm:gap-y-6 md:gap-y-8">
+          <div className="flex flex-col sm:items-center gap-y-5">
+            <div className="w-fit flex justify-center items-center pl-3 sm:pl-0">
+              <div className="absolute size-[86px] sm:size-[94.6] lg:size-[107.5] rounded-full border-4 border-primary border-double animate-spin" style={{animationDuration: "3s"}}/>
+              <Image
+                src={siteConfig.owner.photo}
+                alt="Me"
+                height={66}
+                width={66}
+                priority
+                className="rounded-full aspect-square object-cover border-2 border-primary sm:size-[72.6] lg:size-[82.5]"
+              />
             </div>
-            <p>Driven by curiosity, focused on innovation.</p>
-          </div>
-        </div>
-        <div className="flex gap-3 mt-6">
-          <Button asChild className="rounded-lg">
-            <Link href={siteConfig.owner.resumeUrl} target="blank">Resume <Icons.externLink strokeWidth={2} className="ml-2 h-4 w-4"/></Link>
-          </Button>
-          <Button asChild className="rounded-lg">
-            <Link href={`mailto:${siteConfig.owner.emailAdresse}`}><Icons.mail strokeWidth={2} className="mr-2 h-4 w-4"/> Get In Touch</Link>
-          </Button>
-        </div>
-        <div className="flex items-center gap-3 mt-16">
-          <div className="w-[20px] h-[45px] bg-transparent border-[3px] border-foreground rounded-full relative">
-            <div className="absolute size-2 m-auto rounded-full inset-0 bg-foreground animate-scrollDown"/>
-          </div>
-          <p className="text-xs">Scroll down</p>
-        </div>         
-                    {/* <div className="text-center sm:text-left flex flex-col items-center sm:items-start gap-3">
-            <h1 className="font-bold text-lg sm:text-xl md:text-2xl uppercase">{siteConfig.owner.firstName} <span className="block md:inline-block">{siteConfig.owner.lastName}</span></h1>
-            <h2 className="font-extrabold text-2xl sm:text-3xl md:text-4xl lg:text-5xl uppercase">Cloud DevSecOps <span className="text-secondary">Engineer</span></h2>
-            <div className="text-base text-muted-foreground text-pretty max-w-[50ch] leading-relaxed">
-              <span className="block sm:inline-block">I&apos;m passionate about,</span>
-              <FlipWords words={words} className="font-medium"/> <br />
-            </div>
-            <div className="flex justify-center gap-3">
-              <Button asChild className="rounded-lg">
-                <Link href={siteConfig.owner.resumeUrl} target="blank">Resume <Icons.externLink strokeWidth={2} className="ml-2 h-4 w-4"/></Link>
-              </Button>
-              <Button asChild className="rounded-lg">
-                <Link href={`mailto:${siteConfig.owner.emailAdresse}`}><Icons.mail strokeWidth={2} className="mr-2 h-4 w-4"/> Get In Touch</Link>
-              </Button>
+            <div className="flex gap-2 items-center rounded-lg border-1 bg-muted w-fit px-4 py-1">
+              <div className="bg-green-700 rounded-full size-2"/>
+              <span className="italic text-sm text-muted-foreground">Available for Work</span>
             </div>
           </div>
-          <div className="flex items-center gap-3">
-            <div className="w-[20px] h-[45px] bg-transparent border-[3px] border-foreground rounded-full relative">
-              <div className="absolute size-2 m-auto rounded-full inset-0 bg-foreground animate-scrollDown"/>
+          <div className="flex flex-col sm:items-center gap-y-4">
+            <h1 className="uppercase italic font-bold text-[1.6rem] sm:text-center sm:text-4xl md:text-5xl sm:tracking-wide leading-[1.5]">
+              <span className="block sm:mb-2">Cloud</span> DevSecOps <span className="text-primary">Engineer</span>
+            </h1>
+            <div className="italic text-muted-foreground sm:text-center max-w-[60ch] space-y-1 xl:text-xl">
+              <div>
+                <span>I&apos;m passionate about,</span> <FlipWords words={words} className="p-0 text-sm xl:text-lg font-medium"/>
+              </div>
+              <p>Driven by curiosity, focused on innovation.</p>
             </div>
-            <p className="text-xs">Scroll down</p>
-          </div> 
-          */}
+            <div className="flex gap-3 items-center mt-4">
+              <Button
+                asChild
+                variant="expandIcon"
+                Icon={Icons.download} iconPlacement="right"
+                className="rounded-xl w-fit"
+              >
+                <Link href={siteConfig.owner.resumeUrl} target="blank">Resume</Link>
+              </Button>            
+              <div className="flex items-center gap-3">
+                <div className="w-[20px] h-[45px] bg-transparent border-[3px] border-foreground rounded-full relative">
+                  <div className="absolute size-2 m-auto rounded-full inset-0 bg-foreground animate-scrollDown"/>
+                </div>
+                <p className="text-xs">Scroll down</p>
+              </div>            
+            </div>
+          </div>
+        </BackgroundLines>
       </section>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -94,12 +94,17 @@ const config = {
           "100%": {
             transform: 'translateY(10px)'
           }
-        }
+        },
+        "shine": {
+          from: { backgroundPosition: '200% 0' },
+          to: { backgroundPosition: '-200% 0' },
+        },        
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         "scrollDown": "circle 2s ease-in-out infinite alternate",
+        "shine": "shine 8s ease-in-out infinite",
       },
     },
   },
